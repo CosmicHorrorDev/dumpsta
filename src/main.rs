5fa82ad723fa0ae819907bebfff99029171d24bd
Lovecraftian Horror <LovecraftianHorror@pm.me>
2022-03-28 00:27:02 -0500

    Split up main

--- src/main.rs
+++ src/main.rs
@@ -25,6 +25,7 @@ mod cli;
 mod dialog;
 
 use dialog::{disps, Dialog};
+use ureq::Agent;
 
 #[derive(Debug, Clone)]
 struct CargoRegistry {
@@ -142,11 +143,132 @@ fn err(err: impl std::error::Error + Send + Sync + 'static) -> anyhow::Error {
     err.into()
 }
 
+fn reverse_dependents_for(index: &Index, crate_name: &str) -> Vec<VersionExt> {
+    index
+        .crates_parallel()
+        .filter_map(|maybe_krate| maybe_krate.ok())
+        .map(|krate| krate.highest_version().to_owned())
+        .map(VersionExt::from)
+        .filter(|version| {
+            version
+                .dependencies()
+                .iter()
+                .any(|dep| dep.crate_name() == crate_name)
+        })
+        .collect()
+}
+
+fn get_uninstalled_insta_dependents() -> Result<Vec<VersionExt>> {
+    let spinner = ProgressBar::new(1).with_style(
+        ProgressStyle::default_spinner()
+            .template("{elapsed:>3.green.bold} {spinner:.blue.bold} {msg:!.bold}"),
+    );
+    spinner.set_message("Finding all current crates that use `insta`...");
+    spinner.enable_steady_tick(100);
+    let index = Index::new_cargo_default()?;
+    let uses_insta = reverse_dependents_for(&index, "insta");
+    spinner.finish();
+    Dialog::raw_with_indent(NonZeroUsize::new(1).unwrap()).info_with(
+        "Found {} crates that use `insta`!",
+        disps![uses_insta.len()],
+    );
+
+    // Check to see which ones we already have installed
+    let scan_dialog = Dialog::new("Scanning locally downloaded crates...");
+    let local_crates = LocalCrates::new()?;
+    let config = index.index_config()?;
+    let to_download: Vec<_> = uses_insta
+        .into_iter()
+        .filter(|version| !local_crates.contains(version))
+        .collect();
+    if to_download.len() == 0 {
+        scan_dialog.info("No crates to download!");
+    } else {
+        scan_dialog.info_with("{} crates to download", disps![to_download.len()]);
+    }
+
+    Ok(to_download)
+}
+
+fn download_crate(agent: &Agent, registry: &CargoRegistry, url: &str) -> Result<String> {
+    // Download file
+    let resp = agent.get(url).call()?;
+    let file_name = resp.get_url().rsplit_once('/').unwrap().1.to_owned();
+    let dl_path = registry.cache().join(&file_name);
+    let mut dl_file = File::create(&dl_path)?;
+    let mut reader = BufReader::new(resp.into_reader());
+    io::copy(&mut reader, &mut dl_file)?;
+
+    // Extract contents
+    let reader = File::open(&dl_path)?;
+    let decompressor = GzDecoder::new(BufReader::new(reader));
+    let mut archive = Archive::new(decompressor);
+    archive.unpack(&registry.src())?;
+
+    Ok(file_name)
+}
+
+fn download_crates(urls: &[String]) -> Result<()> {
+    let counter = format!(
+        "{}{{pos:.cyan.bold}}{}{{len:.cyan.bold}}{}",
+        "(".cyan().bold(),
+        "/".cyan().bold(),
+        ")".cyan().bold(),
+    );
+    let eta = format!("{} {{eta:<3.green.bold}}", "eta".green().bold());
+    let pb = ProgressBar::new(urls.len() as u64).with_style(
+        ProgressStyle::default_bar()
+            .template(&format!(
+                "{} {} {}{{bar:60.blue}}{} {}",
+                "Downloading:".bold(),
+                counter,
+                "|".bold(),
+                "|".bold(),
+                eta,
+            ))
+            .progress_chars("█▉▊▋▌▍▎▏ "),
+    );
+    let full_dl_dialog = Dialog::new("Downloading crates...");
+    let cargo_registry = CargoRegistry::new()?;
+    let agent = ureq::builder()
+        // Setting a description user agent per crates.io crawling policy
+        .user_agent("dumpsta (github.com/LovecraftianHorror/dumpsta)")
+        .build();
+    let mut num_install_errors = 0;
+    for url in pb.wrap_iter(urls.iter()) {
+        // Performing at most one request per second per crates.io crawling policy
+        sleep(Duration::from_secs(1));
+        let (crate_dl_dialog, msg) = full_dl_dialog.info_str_with("Downloading {}...", disps![url]);
+        pb.println(msg);
+
+        match download_crate(&agent, &cargo_registry, &url) {
+            Ok(file_name) => {
+                let (_, msg) = crate_dl_dialog.msg_str_with(
+                    Color::Green,
+                    "Downloaded and extracted {}",
+                    disps![file_name],
+                );
+                pb.println(msg);
+            }
+            Err(e) => {
+                crate_dl_dialog.warn_with("Failed download for {}, Err: {}", disps![url, e]);
+                num_install_errors += 1;
+                continue;
+            }
+        }
+    }
+
+    if num_install_errors != 0 {
+        full_dl_dialog.warn_with("Failed pulling {} crates", disps![num_install_errors]);
+    }
+
+    Ok(())
+}
+
 // TODO: check cached and extracted files
 // TODO: Add a flag for force updating the index
 // - Have this store a flag and limit. We don't need people to force updates all the time
 // TODO: display the error with our `Dialog` stuff
-// TODO: spinner on potentially pulling the index?
 // TODO: Add a check to avoid scanning the full index
 // - A simple timestamp on the last check should be enough
 // TODO: Have a default out dir and an option to override
@@ -158,160 +280,20 @@ fn main() -> Result<()> {
         .num_threads(threads)
         .build_global()?;
 
-    let spinner = ProgressBar::new(1).with_style(
-        ProgressStyle::default_spinner()
-            .template("{elapsed:>3.green.bold} {spinner:.blue.bold} {msg:!.bold}"),
-    );
-    spinner.set_message("Finding all current crates that use `insta`...");
-    spinner.enable_steady_tick(100);
-
-    // Get any new versions we haven't seen before
+    let to_download = get_uninstalled_insta_dependents()?;
     let index = Index::new_cargo_default()?;
-    let new_versions = index
-        .crates_parallel()
-        .filter_map(|maybe_krate| maybe_krate.ok())
-        .map(|krate| krate.highest_version().to_owned())
-        .map(VersionExt::from);
-
-    let uses_insta: Vec<_> = new_versions
-        .filter(|version| {
-            version
-                .dependencies()
-                .iter()
-                .any(|dep| dep.crate_name() == "insta")
-        })
-        .collect();
-
-    spinner.finish();
-    Dialog::raw_with_indent(NonZeroUsize::new(1).unwrap())
-        .info_with("Found {} crates using `insta`!", disps![uses_insta.len()]);
-
-    let scan_dialog = Dialog::new("Scanning locally downloaded crates...");
-    // See if the crate is already downloaded in
-    // $CARGO_HOME/registry/src/github.com-<hash>
-    // If it is then search that, otherwise download it in memory and extract it while filtering
-    // for any `.snap` files
-    let local_crates = LocalCrates::new()?;
     let config = index.index_config()?;
-    let download_urls: Vec<_> = uses_insta
-        .into_iter()
-        .map(VersionExt::from)
-        .filter(|version| !local_crates.contains(version))
+    let download_urls: Vec<_> = to_download
+        .iter()
         .filter_map(|version| version.download_url(&config))
         .collect();
-    if download_urls.len() == 0 {
-        scan_dialog.info("No crates to download!");
-    } else {
-        scan_dialog.info_with("{} crates to download", disps![download_urls.len()]);
-    }
 
     if dry_run {
         Dialog::new("Finished dry run!");
         return Ok(());
     }
 
-    // let urls_iter = download_urls.iter();
-    let pb = ProgressBar::new(download_urls.len() as u64).with_style(
-        ProgressStyle::default_bar()
-            .template(&format!(
-                "{} {}{{pos:.cyan.bold}}{}{{len:.cyan.bold}}{} {}{{bar:60.blue}}{} {} {{eta:<3.green.bold}}",
-                "Downloading:".bold(),
-                "(".cyan().bold(),
-                "/".cyan().bold(),
-                ")".cyan().bold(),
-                "|".bold(),
-                "|".bold(),
-                "eta".green().bold(),
-            ))
-            .progress_chars("█▉▊▋▌▍▎▏ "),
-    );
-    let full_dl_dialog = Dialog::new("Downloading crates...");
-    let cargo_registry = CargoRegistry::new()?;
-    let cache_path = cargo_registry.cache();
-    let src_path = cargo_registry.src();
-    let agent = ureq::builder()
-        // Setting a description user agent per crates.io crawling policy
-        .user_agent("dumpsta (github.com/LovecraftianHorror/dumpsta)")
-        .build();
-    let mut num_install_errors = 0;
-    for url in pb.wrap_iter(download_urls.iter()) {
-        // Performing at most one request per second per crates.io crawling policy
-        sleep(Duration::from_secs(1));
-        let (crate_dl_dialog, msg) = full_dl_dialog.info_str_with("Downloading {}...", disps![url]);
-        pb.println(msg);
-
-        let resp = match agent.get(url).call() {
-            Ok(resp) => resp,
-            Err(e) => {
-                crate_dl_dialog
-                    .warn_with("Error downloading file: {}, Err: {}", disps![url, err(e)]);
-                num_install_errors += 1;
-                continue;
-            }
-        };
-
-        // TODO: combine together creating and downloading the file
-        let file_name = resp.get_url().rsplit_once('/').unwrap().1.to_owned();
-        let dl_path = cache_path.join(&file_name);
-        let mut dl_file = match File::create(&dl_path) {
-            Ok(file) => file,
-            Err(e) => {
-                crate_dl_dialog
-                    .warn_with("Failed creating file: {}, Err: {}", disps![dl_path, err(e)]);
-                num_install_errors += 1;
-                continue;
-            }
-        };
-
-        let mut reader = BufReader::new(resp.into_reader());
-        match io::copy(&mut reader, &mut dl_file) {
-            Ok(_) => {}
-            Err(e) => {
-                crate_dl_dialog.warn_with(
-                    "Failed downloading file: {}, Err: {}",
-                    disps![file_name, err(e)],
-                );
-                num_install_errors += 1;
-                continue;
-            }
-        }
-
-        // TODO: combine together opening and extracting the file
-        let reader = match File::open(&dl_path) {
-            Ok(file) => file,
-            Err(e) => {
-                crate_dl_dialog
-                    .warn_with("Failed opening file: {}, Err: {}", disps![dl_path, err(e)]);
-                num_install_errors += 1;
-                continue;
-            }
-        };
-
-        let decompressor = GzDecoder::new(BufReader::new(reader));
-        let mut archive = Archive::new(decompressor);
-        match archive.unpack(&src_path) {
-            Ok(_) => {
-                let (_, msg) = crate_dl_dialog.msg_str_with(
-                    Color::Green,
-                    "Downloaded and extracted {}",
-                    disps![file_name],
-                );
-                pb.println(msg);
-            }
-            Err(e) => {
-                crate_dl_dialog.warn_with(
-                    "Failed extracting file: {}, Err: {}",
-                    disps![dl_path, err(e)],
-                );
-                num_install_errors += 1;
-                continue;
-            }
-        }
-    }
-
-    if num_install_errors != 0 {
-        full_dl_dialog.warn_with("Failed pulling {} crates", disps![num_install_errors]);
-    }
+    download_crates(&download_urls)?;
 
     Ok(())
 }
